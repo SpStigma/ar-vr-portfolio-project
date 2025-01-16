fc95cbc5619210b4b67bbeb5a50a4bf515e3dfa1
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2025-01-16 23:43:25 +0100

    ADD: lose canva.

---
+++ Heart Of The Totem/Assets/Scripts/Global/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject totem;
+    public GameObject defeatPanel;
+
+    public void Start()
+    {
+        totem = GameObject.FindGameObjectWithTag("Totem");
+    }
+
+    public void Update()
+    {
+        if(totem == null)
+        {
+            defeatPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
